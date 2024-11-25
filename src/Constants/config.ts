ce7a9de5f42566599f57e92911435973b8721490
SudhaRatan <rattankumar2511@gmail.com>
2024-11-25 16:11:30 +0530

    conf

--- src/Constants/config.ts
+++ src/Constants/config.ts
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:3000";
-export const API_URL1 = "https://mailhelpbackend.onrender.com";
+export const API_URL1 = "http://localhost:3000";
+export const API_URL = "https://mailhelpbackend.onrender.com";
 
 export const scopes = [
   "https://www.googleapis.com/auth/gmail.send", // Send emails
